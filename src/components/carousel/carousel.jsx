5c02871cd64d7a605551a5126b8e3aad5d8ae533
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2022-08-16 19:55:10 -0500

    react-carousel added

---
+++ src/components/carousel/carousel.jsx
@@ -0,0 +1,424 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "./carousel.css";
+
+export default class Carousel extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      fade: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+    return (
+      <div>
+        <Slider {...settings}>
+          <div>
+            <img src="./images/bash-1.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-2.jpeg" />
+          </div>
+          <div>
+            <img src="./images/bash-3.jpeg" />
+          </div>
+          <div>
+            <img src="./images/bash-4.jpeg" />
+          </div>
+          <div>
+            <img src="./images/bash-5.jpeg" />
+          </div>
+          <div>
+            <img src="./images/bash-6.jpeg" />
+          </div>
+          <div>
+            <img src="./images/bash-7.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-8.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-9.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-10.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-11.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-12.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-13.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-14.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-15.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-16.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-17.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-18.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-19.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-20.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-21.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-22.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-23.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-24.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-25.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-26.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-27.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-28.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-29.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-30.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-31.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-32.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-33.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-34.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-35.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-36.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-37.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-38.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-39.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-40.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-41.png" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-42.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-43.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-44.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-45.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-46.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-47.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-48.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-49.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-50.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-51.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-52.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-54.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-55.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-56.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-57.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-58.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-59.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-60.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-61.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-62.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-63.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-64.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-65.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-66.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-67.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-68.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-69.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-70.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-71.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-73.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-74.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-75.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-76.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-77.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-78.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-79.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-80.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-81.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-82.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-83.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-84.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-85.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-86.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-87.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-88.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-89.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-90.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-91.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-92.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-93.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-94.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-95.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-97.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-98.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-99.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-100.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-101.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-102.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-103.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-104.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-105.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-106.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-107.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-108.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-109.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-110.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-111.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-112.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-113.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-114.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-115.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-116.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-117.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-118.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-119.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-120.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-121.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-122.jpeg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-123.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-125.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-126.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-127.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-128.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-129.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-130.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-131.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-132.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-133.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-134.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-135.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-136.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-137.jpg" alt="" />
+          </div>
+          <div>
+            <img src="./images/bash-138.jpg" alt="" />
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
